29a48d81cfc19d13618eed5316245e2aaca692fb
Leela Prasad <penumutchu.prasad@gmail.com>
2019-04-24 11:16:11 +0530

    Initial Commit

---
+++ LKParallaxHeader/ViewController.swift
@@ -0,0 +1,25 @@
+//ViewController.swift
+/*
+ * LKParallaxHeader
+ * Created by penumutchu.prasad@gmail.com on 24/04/19
+ * is a product created by abnboys 
+ * For abnboys in the  in the LKParallaxHeader
+ * Here the permission is granted to this file with free of use anywhere in any iOS Projects.
+ * Copyright © 2019 abnboys.com. All rights reserved.
+*/
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+    }
+
+
+}
+
